9a237f5aaa5b2d75045be7fa782859c95b4fabc0
Andrew Bungcayao <thedevandrew@gmail.com>
2022-08-02 22:29:08 -0400

    Update connection.js file for MongoDB Atlas/Heroku deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://abungcayao:rootroot@cluster0.kyzq1.mongodb.net/Book-Search-Engine?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
